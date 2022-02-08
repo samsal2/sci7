0a17b8a3d7b109c08deeba8892ee7f4036c8fdcf
samsal2 <samsal9681@gmail.com>
2022-02-07 21:43:53 -0600

    first commit

---
+++ cincat/280122.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+k_1 = 0
+k_2 = 0
+
+
+def fobj_1(e_1, e_2):
+  n = e_1 * e_1
+  d = np.pow(0.2 - 0.5 * e_1 - 0.5 * e_2) *
+  
+
+def fobj_2(e_1, e_2):
+  n = np.pow(0.3 + 2 * e_2, 2)
+  n = 
